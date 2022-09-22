12c76bd9a7f5a7e8811f8d166c1b6fbc3b5579da
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-09-22 16:28:56 +0700

    alodone

---
+++ alo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> hello ban yeu </h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhinhgaixinh.com%2Fgai-xinh-2k%2Fanh-gai-hoc-sinh%2F&psig=AOvVaw17QX48DBm-BCNgK4VR_6M3&ust=1663925313561000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKikkvyKqPoCFQAAAAAdAAAAABAI">
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 echo "<h1> Hello world </h1>";
 echo "<h2> Deploy code to heroku successfully ! </h2>";
 echo "<h2> Deploy code to heroku successfully ! </h2>";
+echo "<img>"
