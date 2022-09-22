0cef17c4b78a6c1c94c522f677a3e6b5fc61c39b
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-09-22 16:32:17 +0700

    alo

--- alo.php
+++ alo.php
@@ -8,7 +8,12 @@
 </head>
 <body>
     <h1> hello ban yeu </h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/WerCpkTJQTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fsgo48.vn%2Ftai-hinh-gai-xinh&psig=AOvVaw17QX48DBm-BCNgK4VR_6M3&ust=1663925313561000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKikkvyKqPoCFQAAAAAdAAAAABAN">
+    <iframe
+     width="560" height="315"
+      src="https://www.youtube.com/embed/WerCpkTJQTM" 
+      title="YouTube video player" 
+      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+       allowfullscreen>
+    </iframe>
 </body>
 </html>
