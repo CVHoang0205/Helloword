7da3f8e73f452d4df97ef230a6c22ad6ad9dab2f
CVHoang0205 <106986621+CVHoang0205@users.noreply.github.com>
2022-09-22 16:21:52 +0700

    done

--- greenwich.php
+++ greenwich.php
@@ -1,1 +1,2 @@
-echo "<h1> Chao mung cac ban gch1001 </h1>"
+<?php
+ echo "<h1> Chao mung cac ban gch1001 </h1>";
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-?php
+<?php
 echo "<h1> Hello world </h1>";
-echo "<h2> Deploy code to heroku successfully ! </h2>"
-echo "<h2> Deploy code to heroku successfully ! </h2>""
+echo "<h2> Deploy code to heroku successfully ! </h2>";
+echo "<h2> Deploy code to heroku successfully ! </h2>";
